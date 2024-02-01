7285e73d110134bb48a3a7f1aa2414220523f6d2
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-02-01 18:08:27 +0100

    Ejemplo if

---
+++ 04_sintaxis/09_condicional_if.js
@@ -0,0 +1,13 @@
+// IF-ELSE
+let contador = 1;
+
+if (contador === 3) {
+    // se ejecuta si contador es 3
+} else if (contador === 4) {
+    // se ejecuta si contador es 4
+} else {
+    // se ejecuta si contador no es 3 ni 4
+}
+
+// IF abreviado
+let autorizacion = (edad > 18) ? true : false;
