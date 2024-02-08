e6193cf1f581577a0471c3c8c6a10f3c226b1cd8
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-02-08 17:08:20 +0100

    Separado en dos ejemplos

---
+++ 04_sintaxis/12_continue.js
@@ -0,0 +1,12 @@
+let suma = 0;
+
+while (suma < 10) {
+    suma += 1
+    if (suma === 5) continue;
+    alert(suma)
+}
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) continue;
+    alert(i)
+}
