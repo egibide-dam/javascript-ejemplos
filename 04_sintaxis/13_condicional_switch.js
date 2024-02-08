69d226ad2b70356991c3530fd9d73f92dc1383c6
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-02-08 17:19:08 +0100

    Ejemplo switch

---
+++ 04_sintaxis/13_condicional_switch.js
@@ -0,0 +1,15 @@
+let numero = 2;
+
+switch (numero) {
+    case 1:
+        alert('Muy pequeño');
+        break;
+    case 2:
+        alert('¡Exacto!');
+        break;
+    case 3:
+        alert('Muy grande');
+        break;
+    default:
+        alert("No tengo ni idea");
+}
