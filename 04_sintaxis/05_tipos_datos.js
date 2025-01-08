defeb026ef7f2ee789fb6109b598ea61044d199c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-01-08 16:49:59 +0100

    Formato revisado

--- 04_sintaxis/05_tipos_datos.js
+++ 04_sintaxis/05_tipos_datos.js
@@ -3,13 +3,13 @@ let numero = 52;
 numero = -3.14;
 
 // Strings
-let nombre = 'Nora' //Comilla simple
-let saludo = "I'm Nora" //Doble
-saludo = `Hola ${nombre}` //Back-tick
+let nombre = 'Nora' // Comilla simple
+let saludo = "I'm Nora" // Doble
+saludo = `Hola ${nombre}` // Back-tick
 
 let telefono = null
 
 let estudiante = true;
 
 let email;
-alert(email); // muestra "undefined"
+alert(email); // Muestra "undefined"
