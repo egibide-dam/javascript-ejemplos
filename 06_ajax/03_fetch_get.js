a12aac0919462c5f3e3200fe5046d56f92ceb720
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2026-01-16 11:53:11 +0100

    Ejemplo actualizado para Mockoon

--- 06_ajax/03_fetch_get.js
+++ 06_ajax/03_fetch_get.js
@@ -1,3 +1,3 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+fetch('http://localhost:3000/users')
     .then(response => response.json())
     .then(json => console.log(json));
--- 06_ajax/04_fecht_post.js
+++ 06_ajax/04_fecht_post.js
@@ -1,4 +1,4 @@
-fetch('https://reqres.in/api/users/', {
+fetch('http://localhost:3000/users', {
     method: 'POST', headers: {
         'Content-Type': 'application/json'
     }, body: JSON.stringify({
